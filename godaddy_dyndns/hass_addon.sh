bee68cf2a150898c0fe98c8f56a5c37bb6d2e70f
Tym Lipari <2223848+tymlipari@users.noreply.github.com>
2024-01-16 17:56:46 -0800

    Remove hello-world & add a GoDaddy Dynamic DNS addon (#1)

---
+++ godaddy_dyndns/hass_addon.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Parse config options
+
+export CONFIG_PATH=/data/options.json
+
+DOMAIN_NAME=$(bashio::config 'domain_name')
+RECORD_NAME=$(bashio::config 'record_name')
+API_KEY=$(bashio::config 'godaddy_api_key')
+API_SECRET=$(bashio::config 'godaddy_api_secret')
+
+# Run forever (or until we get killed)
+while true; do
+  echo "Checking for DNS updates"
+  ./update_godaddy_record.sh \
+      --domain="$DOMAIN_NAME" \
+      --record="$RECORD_NAME" \
+      --api-key="$API_KEY" \
+      --api-secret="$API_SECRET"
+
+  echo "Waiting 10 minutes until next check"
+  sleep 10m
+done
